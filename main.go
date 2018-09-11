1e36072dbcd33077afa0c79611132a9c3b9b21aa
Mitt <MittWillson@icloud.com>
2018-09-12 02:00:00 +0800

    go fmt

--- main.go
+++ main.go
@@ -1,20 +1,19 @@
 package main
 
 import (
-	"golang.org/x/net/proxy"
-	"fmt"
-	"os"
-	"net"
 	"encoding/binary"
 	"flag"
+	"fmt"
+	"golang.org/x/net/proxy"
+	"net"
+	"os"
 )
 
-
-func main(){
+func main() {
 	var flags struct {
 		Bind        string
 		SocksServer string
-		DNSServer	string
+		DNSServer   string
 	}
 	flag.StringVar(&flags.Bind, "bind", "127.0.0.1:8053", "address to bind")
 	flag.StringVar(&flags.SocksServer, "socks-server", "127.0.0.1:1080", "socks5 server address")
@@ -26,15 +25,14 @@ func main(){
 		fmt.Fprintln(os.Stderr, "can't bind:", flags.Bind, err)
 		os.Exit(1)
 	}
-	for{
+	for {
 		buf := make([]byte, 64*1024)
 		n, addr, _ := localConn.ReadFrom(buf[2:])
 		go handleQuery(flags.DNSServer, dialer, localConn, n, addr, buf)
 	}
 }
 
-
-func handleQuery(dnsServer string, dialer proxy.Dialer, localConn net.PacketConn, n int, addr net.Addr, buf []byte){
+func handleQuery(dnsServer string, dialer proxy.Dialer, localConn net.PacketConn, n int, addr net.Addr, buf []byte) {
 	conn, err := dialer.Dial("tcp", dnsServer)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "connect dns server error:", err)
@@ -44,24 +42,24 @@ func handleQuery(dnsServer string, dialer proxy.Dialer, localConn net.PacketConn
 	binary.BigEndian.PutUint16(buf, uint16(n))
 	_, err = conn.Write(buf[:n+2])
 	if err != nil {
-		fmt.Fprintln(os.Stderr,"write to dns server error:", err)
+		fmt.Fprintln(os.Stderr, "write to dns server error:", err)
 		return
 	}
 	n, err = conn.Read(buf[:2])
 	if err != nil {
-		fmt.Fprintln(os.Stderr,"read length from dns server error:", err)
+		fmt.Fprintln(os.Stderr, "read length from dns server error:", err)
 		return
 	}
 	size := binary.BigEndian.Uint16(buf[:2])
-	resBuf := buf[2:2+size]
+	resBuf := buf[2 : 2+size]
 	n, err = conn.Read(resBuf)
 	if err != nil {
-		fmt.Fprintln(os.Stderr,"read data from dns server error:", err)
+		fmt.Fprintln(os.Stderr, "read data from dns server error:", err)
 		return
 	}
 	n, err = localConn.WriteTo(resBuf, addr)
 	if err != nil {
-		fmt.Fprintln(os.Stderr,"back write error %s", err.Error())
+		fmt.Fprintln(os.Stderr, "back write error %s", err.Error())
 		return
 	}
 }
